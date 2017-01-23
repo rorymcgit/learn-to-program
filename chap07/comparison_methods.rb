a387e098c45b8dcf937b1ffd2c057955c39888c1
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-23 15:29:01 +0000

    comparison methods true false

---
+++ chap07/comparison_methods.rb
@@ -0,0 +1,19 @@
+puts 1 > 2
+puts 1 < 2
+puts
+
+puts 5 >= 5
+puts 5 <= 4
+puts
+
+puts 1 == 1
+puts 2 != 1
+puts
+
+puts 'cat' < 'dog'
+puts 'bug lady'          < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
+puts
+
+puts 2   < 10
+puts '2' < '10' # false because '10' is string of 1 0
