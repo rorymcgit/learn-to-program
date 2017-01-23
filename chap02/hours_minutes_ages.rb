457b256f1635cbcd3d745fa8f713d092264832c3
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-23 10:07:30 +0000

    chap01-02

---
+++ chap02/hours_minutes_ages.rb
@@ -0,0 +1,15 @@
+# How many hours are in a year?
+puts "There are #{24*365} hours in a year."
+
+
+# How many minutes are in a decade?
+puts "There are #{24*365*60*10} minutes in a decade."
+
+
+# How many seconds old are you?
+seconds_in_a_day = 24*60*60
+puts "I am #{24*365*60*60*28-(2*seconds_in_a_day)} seconds old."
+
+# Authors age
+auth_age_seconds = 1160000000
+puts "Author is #{auth_age_seconds/60/60/365/24} years old."
