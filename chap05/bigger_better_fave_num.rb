e4b0916dbd9e71c002112afad74703cef9e3f5dd
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-23 12:02:39 +0000

    chap05 done

---
+++ chap05/bigger_better_fave_num.rb
@@ -0,0 +1,3 @@
+puts 'What is your favourite number?'
+fave_num = gets.chomp
+puts 'Don\'t you think ' + (fave_num.to_i + 1).to_s + ' is better?'
---
+++ chap05/full_name_greeting.rb
@@ -0,0 +1,8 @@
+puts 'Hello, what\'s your first name?'
+first_name = gets.chomp
+puts 'And your middle name?'
+middle_name = gets.chomp
+puts 'And your last name?'
+last_name = gets.chomp
+
+puts 'Hello ' + first_name + ' ' + middle_name + ' ' + last_name + '.'
