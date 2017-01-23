1301adf4cdc8d9636bc432ebdcf01dae348e4f1a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-23 11:31:22 +0000

    chap03-04 complete

---
+++ chap03/Letters.rb
@@ -0,0 +1,31 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.'
+
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+#puts '12' + 12 # => string + fixnum doesn't work
+#puts '2' * '5' # => cannot multiply strings
+
+puts 'pig' * 5
+#puts 5 * 'pig' # => cannot print 5 'pig' times
+
+puts 'You\'re swell!'
+puts "You're swell!"
+
+puts 'backlash at the end of string: \\'
+puts 'up\\down'
+puts 'up\down'
---
+++ chap04/variables_and_assignment.rb
@@ -0,0 +1,31 @@
+my_string = '...you can say that again'
+puts my_string
+puts my_string
+
+name = 'Anya Christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow!  ' + name
+puts 'is a really long name!'
+
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+puts my_own_var.class
+
+my_own_var = 5*(1+2)
+puts my_own_var
+puts my_own_var.class
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1#.class
+puts var2#.class
