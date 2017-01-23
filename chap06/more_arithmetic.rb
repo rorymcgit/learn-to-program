5261c3870e01e76af01438ecdb4fbf2cb2a2a36b
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-23 15:15:50 +0000

    chap06 maths

---
+++ chap06/more_arithmetic.rb
@@ -0,0 +1,9 @@
+puts 5**2   # => 25
+puts 5**0.5 # SQUARE ROOT OF 5
+# puts 2.236067**2 # => ~5
+puts 7/3    # => 2
+puts 7%3    # => 1 left over
+puts 365%7  # => 1 left over
+
+puts (5-2).abs
+puts (2-5).abs
---
+++ chap06/random_numbers.rb
@@ -0,0 +1,30 @@
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(1))
+puts(rand(1))
+puts(rand(1))
+puts(rand(999999999999999999999))
+puts('The weatherman said there is a ' + rand(101).to_s + '% chance of rain,')
+puts('but you can never trust a weatherman.')
+
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts
+srand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
