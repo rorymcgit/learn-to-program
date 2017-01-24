5fce04ce1c47554f77765eaab49937a90deaf720
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-24 10:28:17 +0000

    last length pop push

---
+++ more_array_methods.rb
@@ -0,0 +1,27 @@
+foods = ['artichoke', 'brioche', 'caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(' ')
+puts foods.join(' :) ') + ' 8)'
+
+200.times do
+  puts []
+end
+
+foods2d = [foods, 'ham', 'cheese']
+puts foods2d.to_s
+
+favourites = []
+favourites.push 'raindrops on roses'
+favourites.push 'whisky on kittens'
+
+puts favourites[0]
+puts favourites.last
+puts favourites.length
+
+puts favourites.pop
+puts favourites
+puts favourites.length
