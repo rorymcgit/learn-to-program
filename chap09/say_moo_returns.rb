dba37268c62d86f3221db81dcf43bda0b4653c9a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-24 11:30:56 +0000

    say moo

---
+++ chap09/say_moo_returns.rb
@@ -0,0 +1,10 @@
+def say_moo(number_of_moos)
+  puts 'mooooooo...'*number_of_moos
+  'yellow submarine'
+end
+
+whats_returned = say_moo(5)
+
+# say_moo(5)
+
+puts whats_returned #=> last expression in method
