ced7cace5218f9b9fac80e8004569a75e6775231
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-27 11:49:04 +0000

    added cheat method and recursion to die class

--- chap13/die_class.rb
+++ chap13/die_class.rb
@@ -1,12 +1,47 @@
 class Die
+  def initialize
+    puts "Cheat or roll? Type your answer:"
+    response = gets.chomp
+    if response.downcase == "cheat"
+      cheat
+    elsif response.downcase == "roll"
+      roll
+    else
+      puts "Not a valid answer. Type in 'cheat' or 'roll'."
+      initialize
+    end
+  end
+
   def roll
-    1 + rand(6)
+    @number_showing = 1 + rand(6)
+  end
+
+  def showing
+    @number_showing
+  end
+
+  def cheat
+    puts "Pick a number 1 - 6"
+    cheat_response = gets.chomp.to_i
+    if (cheat_response <= 6) && (cheat_response >= 1)
+      @number_showing = cheat_response
+    else
+      puts "Not a valid number!"
+      cheat
+    end
+    @number_showing
   end
 end
 
 
-dice = [Die.new, Die.new, Die.new, Die.new]
+die = Die.new
+puts "Result of rolled die:"
+puts die.showing
 
-dice.each do |die|
-  puts die.roll
-end
+# die.roll
+# puts die.showing
+# puts die.showing
+
+# die.roll
+# puts die.showing
+# puts die.showing
