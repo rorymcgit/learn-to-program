0739febdf6651ec81e5d0acde35c6bebac8698f9
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-27 09:22:00 +0000

    billion seconds

---
+++ chap12/billion_seconds.rb
@@ -0,0 +1,11 @@
+moment_born = Time.local(1989, 1, 25, 3)
+puts moment_born
+
+current_time = Time.new
+puts current_time
+
+seconds_old = current_time - moment_born
+
+time_left = 1000000000 - seconds_old
+
+puts current_time + time_left # I turn 1 billion seconds old in 2020!
