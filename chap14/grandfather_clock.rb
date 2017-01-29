3982bda29f4a16b7c38e3c838843803bbdaa709a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-29 16:03:59 +0000

    grandfather clock

---
+++ chap14/grandfather_clock.rb
@@ -0,0 +1,12 @@
+def grandfather_clock(&block)
+  if Time.new.hour == 0
+    hour = 12
+  elsif Time.new.hour > 12
+    hour = Time.new.hour - 12
+    # puts hour
+  end
+  hour.times {block.call}
+end
+
+grandfather_clock { puts 'DONG' }
+grandfather_clock { puts 100/5 }
