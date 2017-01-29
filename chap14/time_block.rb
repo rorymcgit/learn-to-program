af676b7b0fe7a750cecf87ebdc8a5dc7fbb06a13
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-29 15:00:07 +0000

    time block updated with true/false option

--- chap14/time_block.rb
+++ chap14/time_block.rb
@@ -1,8 +1,13 @@
 def profile (block_description, &block)
-  start_time = Time.new
-  block.call
-  duration = Time.new - start_time
-  puts "#{block_description}: #{duration} seconds"
+  run_profile = true
+  if run_profile
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
 
 profile '25000 doublings' do
